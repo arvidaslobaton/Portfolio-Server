b5d1d7447a78169dde6b4a11743ed80d0db34471
arvidaslobaton <arvidaslobaton@gmail.com>
2024-11-05 10:27:09 +0800

    feat: render

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,13 +1,30 @@
 export default [
   "strapi::logger",
   "strapi::errors",
-  "strapi::security",
+  {
+    name: "strapi::security",
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          "connect-src": ["'self'", "https:"],
+          "img-src": ["'self'", "data:", "blob:", "https:"],
+          "media-src": ["'self'", "data:", "blob:", "https:"],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
   {
     name: "strapi::cors",
     config: {
-      enabled: true,
-      headers: "*",
-      origin: ["http://localhost:3000"],
+      origin: [
+        "http://localhost:3000",
+        "https://your-production-frontend-url.com",
+      ],
+      methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "HEAD", "OPTIONS"],
+      headers: ["Content-Type", "Authorization", "Origin", "Accept"],
+      keepHeaderOnError: true,
     },
   },
   "strapi::poweredBy",
